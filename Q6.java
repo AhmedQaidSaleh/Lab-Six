f2d9772f22cfcb1e43f86fb71b134eb0a888a7ec
AhmedQaidSaleh <111248415+AhmedQaidSaleh@users.noreply.github.com>
2022-10-14 16:39:38 +0300

    Add files via upload

---
+++ Q6.java
@@ -0,0 +1,18 @@
+public class Q6 {
+    public static void main(String[] args) {
+        int a;
+        int sum =1;
+        for ( a=0;a<21;a++){
+            System.out.println("sum of power "+a+"="+sum);
+            sum=sum*2;
+        }
+        System.out.println("the sum of power "+a+"="+sum);
+    }
+//    public static void main(String[] args) {
+//        int number =1;
+//        if (number<Math.pow(2,20)){
+//            number= 2*number;
+//            System.out.println(""+number);
+//        }
+//    }
+}
